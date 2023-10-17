dc56615af06408ca7fc6d10839fd83d44d4125b7
lxz <lixiaozhui3@gmail.com>
2023-10-17 18:09:10 +0800

    update1

--- exercises/primitive_types/primitive_types5.rs
+++ exercises/primitive_types/primitive_types5.rs
@@ -3,13 +3,28 @@
 // Destructure the `cat` tuple so that the println will work.
 //
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand
-// for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    let (name, age)/* your pattern here */ = cat;
 
     println!("{} is {} years old.", name, age);
+
+
+    let a = [1, 2, 3, 4, 5];
+
+    let nice_slice = &a[1..4];
+
+    assert_eq!([2, 3, 4], nice_slice);
+    assert_eq!(&[2, 3, 4], nice_slice);
+    if [2, 3, 4] == *nice_slice {
+        println!("[2, 3, 4]== nice_slice");
+    }
+    if [2, 3, 4] == nice_slice {
+        println!("[2, 3, 4]== nice_slice");
+    }
+    if &[2, 3, 4] == nice_slice {
+        println!("&[2, 3, 4] == nice_slice");
+    }
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,7 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -30,4 +29,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(p0: i32) -> i32 {
+    if p0 > 40 {
+        p0
+    } else {
+        p0 * 2
+    }
 }
